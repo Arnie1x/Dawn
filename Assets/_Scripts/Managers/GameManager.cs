1c994375a8b0a2b1bffb5338fb17b5e676b40991
Arnie <arnie752@gmail.com>
2023-11-19 02:15:30 +0300

    Added a Game Manager to maintain the game loop

---
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Application = UnityEngine.Device.Application;
+
+public class GameManager: MonoBehaviour
+    {
+        public GameObject player;
+
+        public GameState State { get; private set; }
+
+        public static event Action<GameState> OnGameStateChanged;
+
+        private void Start()
+        {
+            UpdateGameState(GameState.Starting);
+        }
+
+        public void UpdateGameState(GameState newState)
+        {
+            State = newState;
+
+            OnGameStateChanged?.Invoke(newState);
+
+            switch (newState)
+            {
+                case GameState.Starting:
+                    HandleStarting();
+                    break;
+                case GameState.Menu:
+                    HandleMenu();
+                    break;
+                case GameState.Playing:
+                    HandlePlaying();
+                    break;
+                case GameState.Paused:
+                    HandlePaused();
+                    break;
+                case GameState.Dead:
+                    HandleDead();
+                    break;
+                case GameState.Lose:
+                    HandleLose();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+            }
+        }
+
+
+        private void HandleStarting()
+        {
+            UpdateGameState(GameState.Menu);
+        }
+        private void HandleMenu()
+        {
+
+        }
+        private void HandlePlaying()
+        {
+
+        }
+        private void HandlePaused()
+        {
+
+        }
+        private void HandleDead()
+        {
+
+        }
+        private void HandleLose()
+        {
+
+        }
+    }
+
+public enum GameState
+{
+    Starting,
+    Menu,
+    Playing,
+    Paused,
+    Dead,
+    Lose
+}
