53d25115d290a92af00270b09e7478f70eca07e4
Arnie <arnie752@gmail.com>
2023-11-19 13:53:54 +0300

    Added AudioSystem.cs
    Added an Audio Manager to play sounds from

---
+++ Assets/_Scripts/Systems/Audio/AudioSystem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSystem : Singleton<AudioSystem>
+{
+    // [SerializeField] private AudioMixer audioMixer;
+
+    [SerializeField] private AudioSource _musicSource;
+    [SerializeField] private AudioSource _SFXSource;
+
+    private void Start()
+    {
+        // audioMixer.SetFloat("SFX", -15f);
+        // audioMixer.SetFloat("Music", -20f);
+    }
+
+    public void PlayMusic (AudioClip clip)
+    {
+        _musicSource.clip = clip;
+        _musicSource.Play();
+    }
+
+    public void PlaySound(AudioClip clip, float vol = 1)
+    {
+        _SFXSource.PlayOneShot(clip, vol);
+    }
+}
